087cee2759de9a9768cfc7f69dabc3ad102514d4
daux2a <daux2a@googlemail.com>
2013-01-17 21:20:20 +0100

    This check all incoming traffic with connected buddys.

---
+++ data/src/core/BuddyIncoming.java
@@ -0,0 +1,162 @@
+package core;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import fileTransfer.FileTransfer;
+
+public class BuddyIncoming {
+	public static void init(String in, Buddy b)	
+	{
+		
+		// Fix filedata Problems and make it saver
+		String save = in.split(" ")[0].replaceAll("[^a-zA-Z_]", "");
+		
+		
+		if (save.equals("status")) {in_status(in, b);}
+		else if (save.equals("ping")) {in_ping(in,b);} 
+		else if (save.equals("pong")) {in_pong(in,b);} 
+		else if (save.equals("profile_name")) {in_profile_name(in,b);} 
+		else if (save.equals("client")) {in_client(in,b);} 
+		else if (save.equals("version")) {in_version(in,b);} 
+		else if (save.equals("profile_text")) {in_profile_text(in,b);} 
+		else if (save.equals("add_me")) {in_add_me(in,b);} 
+		else if (save.equals("remove_me")) {in_remove_me(in,b);} 
+		else if (save.equals("message")) {in_message(in,b);} 
+		else if (save.equals("not_implemented")) {in_not_implemented(in,b);} 
+		else if (save.equals("profile_avatar")) {in_profile_avatar(in,b);}
+		else if (save.equals("filedata_ok")) {FileTransfer.in_filedata_ok(b,in);}
+		else if (save.equals("filedata_error")) {FileTransfer.in_filedata_error(b,in);}
+		else if (save.equals("file_stop_sending")) {FileTransfer.in_file_stop_sending(b,in);}
+		else if (save.equals("file_stop_receiving")) {FileTransfer.in_file_stop_receiving(b,in);}
+		else {in_nothing(in, b);}
+	}
+	
+    // Why there are two incoming streams?
+	public static void init_outin(String in, Buddy b, InputStream c)	
+	{
+		// Fix filedata Problems and make it saver
+		String save = in.split(" ")[0].replaceAll("[^a-zA-Z_]", "");
+
+		if (save.equals("filename")) {FileTransfer.in_filename(b,in,c);}
+		else if (save.equals("filedata")) {FileTransfer.in_filedata(b,in,c);}
+
+	}
+	
+	
+	private static void in_status(String in, Buddy b)
+	{
+		b.lastStatusRecieved = System.currentTimeMillis();
+		byte nstatus = in.split(" ")[1].equalsIgnoreCase("available") ? Buddy.ONLINE : in.split(" ")[1].equalsIgnoreCase("xa") ? Buddy.XA : in.split(" ")[1].equalsIgnoreCase("away") ? Buddy.AWAY : -1;
+		b.setStatus(nstatus); // checks for change in method
+	}
+	private static void in_profile_name(String in, Buddy b)
+	{
+		String old = b.profile_name;
+        b.profile_name = in.split(" ", 2)[1];
+		APIManager.fireProfileNameChange(b, b.profile_name, old);
+	}
+	private static void in_client(String in, Buddy b)
+	{
+		b.client = in.split(" ", 2)[1];
+	}
+	private static void in_version(String in, Buddy b)
+	{
+		b.version = in.split(" ", 2)[1];
+	}
+	private static void in_profile_text(String in, Buddy b)
+	{
+		String old = b.profile_text;
+		b.profile_text = in.split(" ", 2)[1];
+		APIManager.fireProfileTextChange(b, b.profile_text, old);
+	}
+	private static void in_add_me(String in, Buddy b)
+	{
+		APIManager.fireAddMe(b);
+	}
+	private static void in_remove_me(String in, Buddy b)
+	{
+		APIManager.fireRemove(b);
+	}
+	private static void in_message(String in, Buddy b)
+	{
+		APIManager.fireMessage(b, in.split(" ", 2)[1]);
+	}
+	private static void in_not_implemented(String in, Buddy b)
+	{
+		Logger.log(Logger.NOTICE, b, "Recieved " + in.trim() + " from " + b.address);
+	}
+	private static void in_profile_avatar(String in, Buddy b)
+	{
+		Logger.log(Logger.NOTICE, b, "Sorry, we have no avatar support. Coming soon.");
+	}
+	private static void in_nothing(String in, Buddy b)
+	{
+		Logger.log(Logger.WARNING, b, "Recieved unknown from " + b.address + " " + in);
+		try {
+			b.sendRaw("not_implemented ");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	private static void in_pong(String in, Buddy b)
+	{
+		if (in.split(" ")[1].equals(b.cookie)) {
+			b.unansweredPings = 0;
+			b.recievedPong = true;
+			Logger.log(Logger.NOTICE, b, b.address + " sent pong");
+			if (b.ourSock != null && b.ourSockOut != null && b.status > Buddy.OFFLINE)
+			{try {
+				b.onFullyConnected();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}}
+			else {
+				Logger.log(Logger.SEVERE, b, "[" + b.address + "] - :/ We should be connected here. Resetting connection!");
+				try {
+					b.disconnect();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				b.connect();
+				return;
+			}
+		} else {
+			Logger.log(Logger.SEVERE, b, "!!!!!!!!!! " + b.address + " !!!!!!!!!! sent us bad pong !!!!!!!!!!");
+			Logger.log(Logger.SEVERE, b, "!!!!!!!!!! " + b.address + " !!!!!!!!!! ~ Disconnecting them");
+			try {
+				b.disconnect();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+
+private static void in_ping(String in, Buddy b)
+{
+		if (b.ourSock == null){b.connect();}
+		try {
+			try {
+				b.sendPong(in.split(" ")[2]);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} catch (NullPointerException npe) {
+			try {
+				b.disconnect();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+}
+	
+	
+	
+}
