d52fb7691c7a68e05770fcc8c4ae5e57edaa3e91
daux2a <daux2a@googlemail.com>
2013-01-25 17:56:57 +0100

    New check if run from source or with jtorchat.jar When there is no data folder in the jtorchat.jar folder it check now the application data folder on windows linux and mac.

--- data/src/util/DataControl.java
+++ data/src/util/DataControl.java
@@ -20,7 +20,10 @@ private static final String CLASS_NAME = TorLoader.class.getName();
 public static void init()
 {
 	Config.BASE_DIR = get_base_pwd();
-	Config.DATA_DIR = "data/";
+	Config.DATA_DIR = "data/";	
+		
+	checkData();
+	
 	Config.CONFIG_DIR = Config.BASE_DIR + Config.DATA_DIR + "config/";
 	Config.DOWNLOAD_DIR =  Config.BASE_DIR + Config.DATA_DIR + "downloads/";
 	Config.LOG_DIR =  Config.BASE_DIR + Config.DATA_DIR + "log/";
@@ -29,10 +32,10 @@ public static void init()
 	Config.TOR_DIR = Config.BASE_DIR + Config.DATA_DIR + "Tor/";
 	Config.LANG_DIR = Config.BASE_DIR + Config.DATA_DIR + "lang/";
 	Config.ICON_DIR_MAIN = Config.BASE_DIR + Config.DATA_DIR + "icon/";	
-	Config.BUDDY_DIR = Config.BASE_DIR + Config.DATA_DIR + "buddy/";	
-		
-	checkData();
+	Config.BUDDY_DIR = Config.BASE_DIR + Config.DATA_DIR + "buddy/";
 	
+	
+	Logger.log(Logger.NOTICE, CLASS_NAME, "Using " + Config.BASE_DIR + " as BASE_DIR");
 	Logger.log(Logger.NOTICE, CLASS_NAME, "Using " + Config.CONFIG_DIR + " as CONFIG_DIR");
 	Logger.log(Logger.NOTICE, CLASS_NAME, "Using " + Config.DOWNLOAD_DIR + " as DOWNLOAD_DIR");
 	Logger.log(Logger.NOTICE, CLASS_NAME, "Using " + Config.LOG_DIR + " as LOG_DIR");
@@ -42,13 +45,12 @@ public static void init()
 	Logger.log(Logger.NOTICE, CLASS_NAME, "Using " + Config.LANG_DIR + " as LANG_DIR");
 	Logger.log(Logger.NOTICE, CLASS_NAME, "Using " + Config.BUDDY_DIR + " as BUDDY_DIR");
 
-	
 }
 
 public static String get_base_pwd() {
 	String os = System.getProperty("os.name").toLowerCase();
     String BASE_DIR = null;
-	
+    
 	try {
 		if (TCPort.extern_source_path.length > 0)
 		{
@@ -59,14 +61,13 @@ public static String get_base_pwd() {
 		{
 		String path = TCPort.class.getProtectionDomain().getCodeSource().getLocation().getPath();
 		BASE_DIR = URLDecoder.decode(path, "UTF-8"); 
-		Logger.log(Logger.NOTICE, "ConfigWriter", BASE_DIR);
+		Logger.log(Logger.NOTICE, CLASS_NAME, "Using " + BASE_DIR + " as CONFIG_DIR");
 
 		if (os.indexOf("win") >= 0) { BASE_DIR=BASE_DIR.split("/", 2)[1]; }
+
 		
-		File f1 = new File(BASE_DIR);
-		
-		// is the destination a file then delete this part from the string
-	    if(f1.isFile())
+		// Different String manipulation for run from source or Jar
+	    if(!BASE_DIR.endsWith("/data/src/output/"))
 	    {
 	    BASE_DIR=Util.reverse(BASE_DIR);
 	    BASE_DIR=BASE_DIR.split("/", 2)[1]; 
@@ -90,8 +91,22 @@ public static String get_base_pwd() {
 //Check consistent of the Data Folder --> not complete
 public static void checkData()
 {
-final String controlFilePath = Config.BASE_DIR+Config.DATA_DIR+Config.controlfile;
+String controlFilePath = Config.BASE_DIR+Config.DATA_DIR+Config.controlfile;
 boolean exists = (new File(controlFilePath)).exists(); // check base dir
+
+
+// When it can not found a config data then use the application data folder to search for the controlfile
+if(!exists)
+{
+Config.BASE_DIR=defaultDirectory()+"/";
+Config.DATA_DIR = ".jtorchat/";	
+Logger.log(Logger.NOTICE, CLASS_NAME, "Use Homefolder for Data because there is not a local data folder.");
+
+controlFilePath = Config.BASE_DIR+Config.DATA_DIR+Config.controlfile;
+exists = (new File(controlFilePath)).exists(); // check base dir
+}
+
+
 if (!exists) {
 	Logger.log(Logger.WARNING, CLASS_NAME, "Wrong base dir, the controlfile is not found: " + controlFilePath);
 	final GuiTorLoading tl = new GuiTorLoading();
@@ -111,5 +126,16 @@ try {
 }
 }} 
 
-
+private static String defaultDirectory()
+{
+    String OS = System.getProperty("os.name").toUpperCase();
+    if (OS.contains("WIN"))
+        return System.getenv("APPDATA");
+    else if (OS.contains("MAC"))
+        return System.getProperty("user.home") + "/Library/Application "
+                + "Support";
+    else if (OS.contains("NUX"))
+        return System.getProperty("user.home");
+    return System.getProperty("user.dir");
+}
 }
